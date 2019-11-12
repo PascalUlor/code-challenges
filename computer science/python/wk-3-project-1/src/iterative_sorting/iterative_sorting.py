5f7e7447b8a5595536d87041b0e7bceb3f2bd8ec
PascalUlor <pascalulor@yahoo.com>
2019-11-12 03:35:32 +0100

    implement count sort

--- computer science/python/wk-3-project-1/src/iterative_sorting/iterative_sorting.py
+++ computer science/python/wk-3-project-1/src/iterative_sorting/iterative_sorting.py
@@ -11,7 +11,7 @@ def selection_sort(arr):
                 smallest_index = j
 
         # TO-DO: swap
-        arr[cur_index],arr[smallest_index] = arr[smallest_index], arr[cur_index]
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
     return arr
 
@@ -29,5 +29,59 @@ def bubble_sort(arr):
 
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
+    """
+    count = array of k+1 zeros
+    for x in input:
+        count[key(x)] += 1
+
+    total = 0
+    for i in 0, 1, ... k:
+        count[i], total = total, count[i] + total
+
+    output = array of the same length as input
+    for x in input:
+        output[count[key(x)]] = x
+    count[key(x)] += 1 
+
+    return output
+    """
+    if len(arr) == 0:
+        return arr
+    # k is the maximum number in array
+    # k + 1; // the length of the array containing counts
+    maximum = max(arr)
+    k = maximum
+    # initialize count array // it stores counts with indexes from 0 to k-1
+    count_array = [0]*(k+1)
+ 
+    # populate count array using the count of each array value as its index
+    for i in range(0, len(arr)):
+        count_array[arr[i]] += 1
+
+    print('==init==', count_array)
+ 
+    # take cumulative sum from of each value in count array
+    for i in range(1, k+1, 1):
+        count_array[i] = count_array[i] + count_array[i-1]
+ 
+    print('==final==', count_array)
+    
+   # initialize sorted/output array
+    sorted_array = [0]*len(arr)
+ 
+    # populate sorted array using the values from count array as its index and
+    # placing the values from main array in each index.
+    # decrement count array by 1 in each iteration
+    for i in range(len(arr)-1, -1, -1):
+        print('==final==', count_array[arr[i]] ,'=====', arr[i])
+        count_array[arr[i]] -= 1
+        sorted_array[count_array[arr[i]]] = arr[i]
+        
+    # set main array equal to sorted array
+    for i in arr:
+        if i < 0:
+            sorted_array = 'Error, negative numbers not allowed in Count Sort'
+    arr = sorted_array
+    print('==final+++++==',sorted_array)
 
     return arr
