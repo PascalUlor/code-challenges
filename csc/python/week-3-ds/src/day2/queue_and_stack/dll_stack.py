d46a3382b264367203fb539f8eb20b1b834635e6
PascalUlor <pascalulor@yahoo.com>
2019-12-02 09:47:30 +0100

    solve linear and binary search

--- csc/python/week-3-ds/src/day2/queue_and_stack/dll_stack.py
+++ csc/python/week-3-ds/src/day2/queue_and_stack/dll_stack.py
@@ -7,12 +7,19 @@ class Stack:
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        # pass
+        self.storage.add_to_head(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        # pass
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        # pass
+        return self.size
--- csc/python/wk-3-project-1/src/searching/searching.py
+++ csc/python/wk-3-project-1/src/searching/searching.py
@@ -2,8 +2,10 @@
 def linear_search(arr, target):
   
   # TO-DO: add missing code
-
-   return -1   # not found
+  for i in range(0, len(arr)):
+    if arr[i] == target:
+      return i
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -11,11 +13,26 @@ def binary_search(arr, target):
 
   if len(arr) == 0:
     return -1 # array empty
-    
+  # low and high to keep track of which
+  # part of the list we are searching
   low = 0
   high = len(arr)-1
 
   # TO-DO: add missing code
+  while low <= high:
+    # guess the mid value
+    midpoint = (low + high)//2
+    guess = arr[midpoint]
+    
+    if guess < target:
+      # shift value of low towards/after midpoint if guess is less that target
+      low = midpoint + 1
+    else:
+      # shift value of high towards/after midpoint if guess is greater that target
+      high = midpoint - 1
+      # if target is at midpoint return midpoint
+    if guess == target:
+      return midpoint
 
   return -1 # not found
 
@@ -28,3 +45,11 @@ def binary_search_recursive(arr, target, low, high):
   if len(arr) == 0:
     return -1 # array empty
   # TO-DO: add missing if/else statements, recursive calls
+  guess = arr[middle]
+
+  if guess == target:
+    return middle
+  if guess < target:
+    return binary_search_recursive(arr, target, middle + 1, high)
+  else:
+    return binary_search_recursive(arr, target, low, middle - 1)
